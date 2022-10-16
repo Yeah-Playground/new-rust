7f930e23384112275865fb661c9992e7eb22c462
YEAHx4 <developerstarlight@gmail.com>
2022-10-16 21:02:48 +0900

    lifetime

---
+++ 10.3.lifetime/src/main.rs
@@ -0,0 +1,25 @@
+use std::fmt::Display;
+
+fn main() {
+    println!("Hello, world!");
+}
+
+fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
+    if x.len() > y.len() {
+        return x;
+    } else {
+        return y;
+    }
+}
+
+fn longest_with_an_announcement<'a, T>(x: &'a str, y: &'a str, ann: T) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
+    }
+}
