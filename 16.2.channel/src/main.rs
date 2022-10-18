71beb7106b6069f2f250c266c556c884c1b4c2df
YEAHx4 <developerstarlight@gmail.com>
2022-10-18 21:21:08 +0900

    thrading

---
+++ 16.2.channel/src/main.rs
@@ -0,0 +1,38 @@
+use std::{sync::mpsc, thread, time::Duration};
+
+fn main() {
+    let (tx1, rx) = mpsc::channel();
+    let tx2 = tx1.clone();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("more"),
+            String::from("message"),
+            String::from("for"),
+            String::from("you"),
+        ];
+
+        for val in vals {
+            tx2.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Got: {}", received);
+    }
+}
